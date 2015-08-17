d10c2b6633e1ae591240f5651b41ebce8ccb7500
banunn <brock@brocknunn.com>
2015-08-16 20:06:50 -0500

    Flickr Photos and Controller Created
    Commit includes minimal styling, addition of the core image items
    ordered by title.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+var app = angular.module('myApp', [])
+  .controller('MainCtrl', function($scope, $http) {
+    $http({
+      method: 'JSONP',
+      url: 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&format=json&nojsoncallback=1',
+      params: {
+        'format': 'json',
+        'jsoncallback': 'JSON_CALLBACK'
+      }
+    }).success(function(data) {
+      $scope.data = data.photos;
+      console.log(data.photos);
+      $scope.loading = false;
+    })
 
+    console.log($scope.loading);
+  })
